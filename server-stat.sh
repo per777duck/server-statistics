ba2e0934653a085b9e2d52828d386a0c816d1e78
per777duck <demyan_permyakov@bk.ru>
2026-06-29 22:11:26 +0700

    Added 'memory usage' statistic

--- server-stat.sh
+++ server-stat.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 echo "----------------------------Statistics of the server----------------------------"
-echo "CPU usage: $(top -bn1 | grep 'Cpu(s)' | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{print 100 - $1"%"}')"
+echo "CPU usage:"
+cpu_stats=$(top -bn1 | grep "Cpu(s)" |  sed 's/.*, *\([0-9]*,[0-9]*\)%* id.*/\1/' |  awk '{print 100 - $1"%"}')
+echo "  $cpu_stats"
+
+echo "MEM usage:"
+total=$(free -m | awk 'NR==2{print $2}')
+used=$(free -m | awk 'NR==2{print $3}')
+free=$(free -m | awk 'NR==2{print $7}')
+
+used_percentage=$(echo "scale=1; ${used} * 100 / ${total}" | bc)
+free_percentage=$(echo "scale=1; ${free} * 100 / ${total}" | bc)
+
+echo "  Used: ${used} MB/${total} MB (${used_percentage}%)"
+echo "  Free: ${free} MB/${total} MB (${free_percentage}%)"
